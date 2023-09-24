61b4307d8d88f5157c70ecf3ec4ed707088b66ad
andsnk <127045868+andsnk@users.noreply.github.com>
2023-09-24 21:14:36 +0300

    ref searbar

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import React, { useState } from 'react';
 import css from './Searchbar.module.css';
 import { CiSearch } from 'react-icons/ci';
 import Notiflix from 'notiflix';
@@ -8,47 +8,41 @@ Notiflix.Notify.init({
   distance: '60px',
   opacity: 1,
 });
+const Searchbar = ({ onSubmit }) => {
+  const [query, setQuery] = useState('');
 
-class Searchbar extends Component {
-  state = {
-    query: '',
+  const handleChange = e => {
+    setQuery(e.target.value);
   };
 
-  handleChange = e => {
-    this.setState({ query: e.target.value });
-  };
-
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    const { query } = this.state;
     if (query.trim() === '') {
       return Notiflix.Notify.warning('Enter the text in the search field');
     }
-    this.props.onSubmit(query);
-    this.setState({ query: '' });
+    onSubmit(query);
+    setQuery('');
   };
 
-  render() {
-    return (
-      <header className={css.searchbar}>
-        <form className={css.form} onSubmit={this.handleSubmit}>
-          <button type="submit" className={css.formBtn}>
-            <CiSearch className={css.formIcon} />
-          </button>
+  return (
+    <header className={css.searchbar}>
+      <form className={css.form} onSubmit={handleSubmit}>
+        <button type="submit" className={css.formBtn}>
+          <CiSearch className={css.formIcon} />
+        </button>
 
-          <input
-            className={css.input}
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            value={this.state.query}
-            onChange={this.handleChange}
-          />
-        </form>
-      </header>
-    );
-  }
-}
+        <input
+          className={css.input}
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          value={query}
+          onChange={handleChange}
+        />
+      </form>
+    </header>
+  );
+};
 
 export default Searchbar;
